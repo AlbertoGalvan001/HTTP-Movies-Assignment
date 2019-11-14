09fcf158c8534002914b1c08bdbca8f24603af1a
Alberto Galvan <astrometrics@Unimatrix-Zero.local>
2019-11-14 16:01:16 -0600

    update movie, initial state with useEffect and handleChange, saveMovie axios put

--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -10,4 +10,43 @@ const initialState = {
 
 const UpdateMovie = props => {
     const [movie, setMovie] = useState({ ...initialState });
+
+    useEffect(() => {
+        const movieToUpdate = props.movies.filter(movie => {
+            return movie.id === props.match.params.id
+        })
+        movieToUpdate && setMovie(movieToUpdate)
+    }, [props.match.params.id])
+
+    const handleChange = e => {
+        if (e.target.name === 'stars') {
+            const stars = e.target.value.split('');
+            setMovie({ ...movie, [e.target.name]: e.target.value })
+        }
+    }
+
+    const saveMovie = e => {
+        e.preventDefault();
+        console.log(movie);
+        axios
+            .put(`http:localhost:5000/api/movies/${movie.id}`, movie)
+            .then(response => {
+                console.log(response);
+                props.setMovies(props.movies.map(item => {
+                    if (item.id === movie.id) {
+                        return response.data;
+                    } else {
+                        return item;
+                    }
+                }))
+                props.history.push('/');
+            })
+            .catch(error => {
+                console.log(error.message);
+            })
+        setMovie({ ...initialState });
+    }
+
+
 }
+
